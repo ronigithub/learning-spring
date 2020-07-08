9cbf054ce3d6e18864d37ae4670f76b94e76ab6f
Roni <ronicse2011@gmail.com>
2020-07-08 06:31:46 +0600

    Constructor Injection

--- src/main/java/com/roni/learning/Tyre.java
+++ src/main/java/com/roni/learning/Tyre.java
@@ -1,11 +1,12 @@
 package com.roni.learning;
 
-import org.springframework.stereotype.Component;
-
-//@Component
 public class Tyre
 {
     private String brand;
+
+    public Tyre(String brand) {
+        this.brand = brand;
+    }
 
     public String getBrand() {
         return brand;
