f2f8988540da41a51fce31d5eb40471c1a91bcdf
Roni <ronicse2011@gmail.com>
2020-07-08 06:43:03 +0600

    Autowired Annotation

--- src/main/java/com/roni/App.java
+++ src/main/java/com/roni/App.java
@@ -4,16 +4,18 @@ import com.roni.learning.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // spring.xml can be any name
 
-        Tyre tyre = (Tyre) context.getBean("tyre");
-        System.out.println(tyre);
+        Car obj = (Car) context.getBean("car");
+        obj.drive();
 
 //        Vehicle obj = (Vehicle) context.getBean("car");
 //        obj.drive();
+
+//        Tyre tyre = (Tyre) context.getBean("tyre");
+//        System.out.println(tyre);
+
     }
 }
--- src/main/java/com/roni/learning/Tyre.java
+++ src/main/java/com/roni/learning/Tyre.java
@@ -1,12 +1,11 @@
 package com.roni.learning;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Tyre
 {
     private String brand;
-
-    public Tyre(String brand) {
-        this.brand = brand;
-    }
 
     public String getBrand() {
         return brand;
@@ -18,9 +17,7 @@ public class Tyre
 
     @Override
     public String toString() {
-        return "Tyre{" +
-                "brand='" + brand + '\'' +
-                '}';
+        return "It's working";
     }
 
 }
